2d2c0950c3a09538d92ad8835fe81fb6846c180a
armanhadi <arman.codex@gmail.com>
2024-01-10 14:08:22 +0300

    update

--- src/lib/components/AnnouncementCarousel.svelte
+++ src/lib/components/AnnouncementCarousel.svelte
@@ -17,7 +17,7 @@
 
 	onMount(() => {
 		swipeConfig = {
-			autoplay: true,
+			autoplay: false,
 			delay: 4000,
 			showIndicators: false,
 			transitionDuration: 500,
@@ -40,19 +40,24 @@
 	{#if announcements && announcements.length > 0 && swipeConfig}
 		<Swipe {...swipeConfig}>
 			{#each announcements as slide}
-				<SwipeItem classes="flex flex-row justify-center items-center w-full">
-					<div class="slide flex flex-row justify-start items-start h-3/4 p-10 w-full">
-						<div class="img-con w-4/6 min-w-4/6 rounded-3xl" bind:this={imgCon}>
-							<img
-								src={slide.image_url}
-								alt={slide.title}
-								class="img rounded-3xl w-full h-full object-cover"
-							/>
+				<SwipeItem classes="flex flex-row justify-center items-center w-full h-screen">
+					<div class="slide flex flex-row justify-start items-start w-full h-5/6">
+						<div class="  p-10 flex justify-center items-center h-full" bind:this={imgCon}>
+							<div class="img-con rounded-3xl w-full h-full">
+								<img
+									src={slide.image_url}
+									alt={slide.title}
+									class="img rounded-3xl w-full h-full object-cover"
+								/>
+							</div>
 						</div>
-						<div class="slide-text p-10 flex flex-col justify-start h-full items-start w-2/5">
-							<h1 class="p-0 m-0 text-8xl text-left text-white mb-10">
+						<div
+							class="slide-text text-con flex py-10 flex-col justify-center items-center overflow-hidden"
+						>
+							<div class="title text-left text-white">
 								{slide.title}
-							</h1>
+							</div>
+
 							<p class="text-white mt-4 text-left">
 								{slide.description}
 							</p>
@@ -65,34 +70,20 @@
 </section>
 
 <style>
+	.text-con {
+		display: inline-block;
+		padding-left: 1vw;
+		padding-right: 1vw;
+	}
+	.title {
+		font-size: 4.2vw;
+		display: block;
+		line-height: normal;
+	}
 	.swipeable-item-inner {
 		width: 100%;
 	}
-	.img-con::before,
-	.img::before {
-		content: '';
-		position: absolute;
-		top: -50%;
-		left: -50%;
-		right: -50%;
-		bottom: -50%;
-		background: inherit;
-		border-radius: 25%;
-		z-index: -1;
-		filter: blur(20px);
-	}
 
-	.img-con::before,
-	.img::after {
-		content: '';
-		position: absolute;
-		top: 0;
-		left: 0;
-		right: 0;
-		bottom: 0;
-		background: inherit;
-		clip-path: polygon(15% 0%, 85% 0%, 100% 50%, 85% 100%, 15% 100%, 0% 50%);
-	}
 	section {
 		display: flex;
 		flex-direction: column;
